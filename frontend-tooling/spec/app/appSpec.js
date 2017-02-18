ca5bd28044b928feba4c0d310b9a4d2e17e5334d
Alex Vanston <alex@mrvdot.com>
2017-02-18 18:08:00 -0500

    Fix specs

--- frontend-tooling/spec/app/appSpec.js
+++ frontend-tooling/spec/app/appSpec.js
@@ -3,12 +3,12 @@ describe('App', function () {
     spyOn(console, 'log');
   });
 
-  if('should log a message on startup', function () {
+  it('should log a message on startup', function () {
     app.bootstrap();
     expect(console.log).toHaveBeenCalledWith(app.startupMessage);
   });
 
-    if('should log a message on shutdown', function () {
+  it('should log a message on shutdown', function () {
     app.stop();
     expect(console.log).toHaveBeenCalledWith(app.shutdownMessage);
   });
