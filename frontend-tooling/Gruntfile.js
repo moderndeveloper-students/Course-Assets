8442d25c18d18611179aabd52b15657c8ecc6291
Alex Vanston <alex@mrvdot.com>
2017-02-18 17:22:07 -0500

    Setup grunt sass

--- frontend-tooling/Gruntfile.js
+++ frontend-tooling/Gruntfile.js
@@ -19,7 +19,23 @@ module.exports = function(grunt) {
       }
     },
     jshint: {
-      all: ['Gruntfile.js', 'js/*.js']
+      all: ['Gruntfile.js', 'js/*.js'],
+      options: {
+          asi: true, // Don't worry about missing semicolons
+          undef: true, // Warn about undeclared globals
+          globals: { // Pass in a list of globals we don't want warnings about
+            module: true,
+            require: true,
+            console: true
+          }
+      }
+    },
+    sass: {
+      all: {
+        files: {
+          'styles/style.css': 'sass/style.scss'
+        }
+      }
     }
   });
 
