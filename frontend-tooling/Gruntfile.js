d03492e6a35f96a3b6f7bf81bfba1442ecc9e69b
Alex Vanston <alex@mrvdot.com>
2017-02-18 14:23:41 -0500

    Create gruntfile

---
+++ frontend-tooling/Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Config will go here
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', function () {
+    console.log('Grunt has run')
+  });
+};
