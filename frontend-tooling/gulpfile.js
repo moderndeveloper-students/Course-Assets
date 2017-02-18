f03f43f0a8da34f91c0365fdcb56e39f38c22a3e
Alex Vanston <alex@mrvdot.com>
2017-02-18 15:58:40 -0500

    Install minify css

--- frontend-tooling/gulpfile.js
+++ frontend-tooling/gulpfile.js
@@ -1,4 +1,11 @@
 var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+ 
+gulp.task('minify-css', function() {
+  return gulp.src('styles/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('default', function () {
   console.log('Gulp has run!');
