f91e75a45a286babee54cd474b5bafeff2b09629
Alex Vanston <alex@mrvdot.com>
2017-02-18 14:48:39 -0500

    Add comments

--- frontend-tooling/js/app.js
+++ frontend-tooling/js/app.js
@@ -1,6 +1,11 @@
 (function () {
+  // Message to display
   var hereIGo = 'Houston we have liftoff';
 
+  /**
+   * Initial startup method. 
+   * Initializes app and prepares any necessary configuratio
+  **/
   function bootstrap() {
     console.log(hereIGo);
   }
