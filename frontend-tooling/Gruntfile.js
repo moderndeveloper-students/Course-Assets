9ce89415e893fafd040d962abdd02a9d7ec464f5
Alex Vanston <alex@mrvdot.com>
2017-02-18 17:30:25 -0500

    Setup watch

--- frontend-tooling/Gruntfile.js
+++ frontend-tooling/Gruntfile.js
@@ -21,19 +21,38 @@ module.exports = function(grunt) {
     jshint: {
       all: ['Gruntfile.js', 'js/*.js'],
       options: {
-          asi: true, // Don't worry about missing semicolons
-          undef: true, // Warn about undeclared globals
-          globals: { // Pass in a list of globals we don't want warnings about
-            module: true,
-            require: true,
-            console: true
-          }
+        asi: true, // Don't worry about missing semicolons
+        undef: true, // Warn about undeclared globals
+        globals: { // Pass in a list of globals we don't want warnings about
+          module: true,
+          require: true,
+          console: true
+        }
+      },
+      dev: {
+        force: true
       }
     },
     sass: {
       all: {
         files: {
           'styles/style.css': 'sass/style.scss'
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['jshint:dev'],
+        options: {
+          livereload: true
+        }
+      },
+      sass: {
+        files: ['sass/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload: true
         }
       }
     }
