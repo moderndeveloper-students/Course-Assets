96a53c1a14a0bd8cd47e65d081a3cf0d3165835e
Alex Vanston <alex@mrvdot.com>
2017-03-05 17:54:23 -0500

    Update to use script templates

--- templating/templates.js
+++ templating/templates.js
@@ -2,17 +2,8 @@
   var $users = $('#users');
   var userUrl = 'https://jsonplaceholder.typicode.com/users';
 
-  var template = Handlebars.compile([
-    '{{#each users}}',
-    '<div class="user">',
-      '<h2 class="name">{{name}}</h2>',
-      '<div class="address">',
-        '<p class="street">{{address.suite}} {{address.street}}</p>',
-        '<p class="city-zip">{{address.city}}, {{address.zipcode}}</p>',
-      '</div>',
-    '</div>',
-    '{{/each}}'
-  ].join("\r\n"));
+  var templateStr = $('#users-template').html();
+  var template = Handlebars.compile(templateStr);
 
   var setUsers = function (users) {
     var html = template({ users: users });
