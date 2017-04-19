7dc45471a3d71cd6c237eb74a778e37d2c253995
Alex Vanston <alex@mrvdot.com>
2017-04-19 18:14:28 -0500

    Setup callbacks

--- advanced-jquery/script.js
+++ advanced-jquery/script.js
@@ -1,5 +1,16 @@
 (function ($) {
-var els = $('.hide-me').on('click', 'button', function () {
-  els.fadeOut(400);
-});
+  var toggles, els;
+  
+  toggles = $('.toggle').on('click', 'button', function () {
+    els.fadeIn(400, function () {
+      toggles.addClass('hidden');
+    });
+  });
+
+  els = $('.hide-me').on('click', 'button', function () {
+    els.fadeOut(400, function () {
+      toggles.removeClass('hidden');
+    });
+  });
+
 })(jQuery);
