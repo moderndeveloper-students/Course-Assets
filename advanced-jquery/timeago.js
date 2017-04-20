5795709343d22505d95848e2aa19d7ab22d6dc33
Alex Vanston <alex@mrvdot.com>
2017-04-20 13:36:21 -0500

    Normalize date

--- advanced-jquery/timeago.js
+++ advanced-jquery/timeago.js
@@ -1,23 +1,3 @@
-;(function($) {
+(function ($) {
 
-  var defaults = {
-    // set defaults
-  };
-  
-  function Timeago(element, options) {
-    this.config = $.extend({}, defaults, options);
-    this.element = element;
-    this.init();
-  }
-
-  
-  Timeago.prototype.init = function() {
-    // plugin logic goes here...
-  };
-  
-  $.fn.timeago = function (options) {
-    new Timeago(this, options);
-    return this;
-  };
-  
 })(jQuery);
